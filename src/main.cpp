844bb3c61517f35307b9af900d7bdb082c37fc18
Frank <reppepf@gmail.com>
2024-05-11 00:50:38 +0200

    My node, inferior implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h" 
+#include "rapidjson/writer.h"
+
+using namespace rapidjson;
 using namespace std;
 
-int main(){
-    cout << "Hellow world" << endl;
+string name = "Frank";
+
+string sign(string msg) {
+    Document doc; 
+    doc.Parse<0>(msg.c_str()).HasParseError();
+    
+    Document::AllocatorType& allocator = doc.GetAllocator();
+    doc.AddMember("signature", 0, allocator);
+    assert(doc.HasMember("signature"));
+
+    const char *c = name.c_str();
+    doc["signature"].SetString(rapidjson::StringRef(c));
+
+    StringBuffer buffer;
+    Writer<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer); 
+ 
+    return buffer.GetString();
+}
+
+void callback(const std_msgs::String::ConstPtr& msg) {
+    ROS_INFO("%s", msg->data.c_str());
+    std_msgs::String s_msg;
+    s_msg.data = sign(msg->data.c_str());
+
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<std_msgs::String>("RaptorOUT", 10);
+    pub.publish(s_msg);
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, name);
+
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("RaptorIN", 10, callback);
+    ros::Publisher pub = nh.advertise<std_msgs::String>("RaptorOUT", 10);
+
+    ros::spin();
+
     return 0;
 }
--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-g++ main.cpp -o main; ./main
+
+g++ main.cpp -o main -I/opt/ros/noetic/include -L/opt/ros/noetic/lib -Wl,-rpath,/opt/ros/noetic/lib -lroscpp -lrosconsole -lroscpp_serialization
+
+g++ messenger.cpp -o messenger -I/opt/ros/noetic/include -L/opt/ros/noetic/lib -Wl,-rpath,/opt/ros/noetic/lib -lroscpp -lrosconsole -lrostime -lroscpp_serialization
